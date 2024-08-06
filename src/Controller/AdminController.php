e4f3dfc7ac0d9cffed5b07b57eb2e6870b9729ad
zmejpc <zmejpc@gmail.com>
2024-08-06 11:42:39 +0300

    add cache

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\HttpCache\StoreInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +15,7 @@ use App\Message\CommentMessage;
 use App\Entity\Comment;
 use Twig\Environment;
 
+#[Route('/admin')]
 class AdminController extends AbstractController
 {
     public function __construct(
@@ -21,7 +24,7 @@ class AdminController extends AbstractController
         private Environment $twig
     ) {}
 
-    #[Route('/admin/comment/review/{id}', name: 'review_comment')]
+    #[Route('/comment/review/{id}', name: 'review_comment')]
     public function reviewComment(Request $request, Comment $comment, WorkflowInterface $commentStateMachine): Response
     {
         $accepted = !$request->query->get('reject');
@@ -45,5 +48,17 @@ class AdminController extends AbstractController
             'transition' => $transition,
             'comment' => $comment,
         ]));
+    }
+
+    #[Route('/http-cache/{uri<.*>}', methods: ['PURGE'])]
+    public function purgeHttpCache(KernelInterface $kernel, Request $request, string $uri, StoreInterface $store): Response
+    {
+        if ('prod' === $kernel->getEnvironment()) {
+            return new Response('KO', 400);
+        }
+
+        $store->purge($request->getSchemeAndHttpHost().'/'.$uri);
+
+        return new Response('Done');
     }
 }
